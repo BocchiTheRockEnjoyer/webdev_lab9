51974c79c953da6f170b52cb5a6bde086f8912e6
BocchiTheRockEnjoyer <ronalrajesh@outlook.com>
2024-11-05 22:54:03 -0600

    Added final file to see what I did wrong

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
-function init(){
     var button = document.getElementById('entrybutton');
+  
     function showMeText(){
       var textbox = document.getElementById('entryinput');
       document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Ronal Rajesh: " + textbox.value);
+      alert("Daniel Krieglstein: " + textbox.value);
     }
+  
     button.addEventListener('click', showMeText);
     }
-  window.addEventListener('load', init); 
-window.addEventListener('load', init);
+    
+  window.addEventListener('load', init);
